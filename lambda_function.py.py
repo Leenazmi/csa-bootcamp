b1c460a33dc89dcc117615173d158e935a37a57f
Leenazmi <112614569+Leenazmi@users.noreply.github.com>
2024-02-15 03:58:12 +0300

    Add files via upload

--- lambda_function.py.py
+++ lambda_function.py.py
@@ -5,6 +5,7 @@ import joblib
 import base64
 from flask import Flask, jsonify, request
 import os
+import requests 
 
 def decode(encoded_img):
     aux_path = 'tmp.png'
@@ -24,34 +25,34 @@ app = Flask(__name__)
 
 @app.route('/predict', methods=['POST'])
 def api():
-    #img = decode(request.json)
-    if 'image' not in request.files:
-        print('first if')
-        return "No image"
-    file=request.files['image']
-    if file.filename == '':
-        print("second if")
-        return 'No selected file' 
-    if file:
-        print('inside')
-        file2 = request.files.get('image')
-        print(file2)
-        img_bytes = file2.read()
-        img_path = "../test.jpg"
-        with open(img_path, "wb") as img:
-            img.write(img_bytes)
-            img.close()
-        img = cv2.imread(img_path)
-        #img = decode(file)
-        new_array = cv2.resize(img,(100,100))
-        new_array = np.array(new_array).reshape(1,-1)
-        new_array = new_array/255.0
-        model = joblib.load('svcModel.joblib')
-        response = model.predict(new_array)
-        print(response[0])
-        return jsonify(
-            body= str(response[0])
-        )
+    img = decode(request.json['image'])
+    # if 'image' not in request.files:
+    #     print('first if')
+    #     return "No image"
+    # file=request.files['image']
+    # if file.filename == '':
+    #     print("second if")
+    #     return 'No selected file' 
+    # if file:
+    print('inside')
+    # file2 = request.files.get('image')
+    # print(file2)
+    # img_bytes = file2.read()
+    # img_path = "../test.jpg"
+    # with open(img_path, "wb") as img:
+    #     img.write(img_bytes)
+    #     img.close()
+    # img = cv2.imread(img_path)
+    #img = decode(file)
+    new_array = cv2.resize(img,(100,100))
+    new_array = np.array(new_array).reshape(1,-1)
+    new_array = new_array/255.0
+    model = joblib.load('svcModel.joblib')
+    response = model.predict(new_array)
+    print(response[0])
+    return jsonify(
+        prediction= str(response[0])
+    )
     
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0')
